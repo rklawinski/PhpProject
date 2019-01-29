9d2fe29b5107fc41dfb9a1b1276ca3705e9630ce
Roman <33792116+rklawinski@users.noreply.github.com>
2019-01-29 16:23:36 -0500

    Update database.php

--- database.php
+++ database.php
@@ -3,10 +3,10 @@ class Database
 {
 	// these are not the real passwords
 	// for real passwords, see file in ../database subdirectory
-	private static $dbName = 'crud_tutorial' ; 
-	private static $dbHost = 'localhost' ;
-	private static $dbUsername = 'root';
-	private static $dbUserPassword = 'root';
+	private static $dbName = 'jrklawin355wi19' ; 
+	private static $dbHost = '10.8.30.49' ;
+	private static $dbUsername = 'jrklawin355wi19';
+	private static $dbUserPassword = 'J@cqui8989';
 	
 	private static $cont  = null;
 	
